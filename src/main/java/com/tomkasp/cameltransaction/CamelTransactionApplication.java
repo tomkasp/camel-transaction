cb79a2163d735532ad01bcdb9919b691656408bf
tomkasp@gmail.com <tomkasp@gmail.com>
2018-03-03 09:45:18 +0100

    Initial commit

---
+++ src/main/java/com/tomkasp/cameltransaction/CamelTransactionApplication.java
@@ -0,0 +1,12 @@
+package com.tomkasp.cameltransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelTransactionApplication.class, args);
+	}
+}
